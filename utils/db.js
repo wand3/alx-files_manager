4ac80bb7b095b6f8e75c82136b699a2ec5ad493f
wand3 <godswillbaiyewu@gmail.com>
2023-09-12 22:21:28 -0700

    First API creation

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,10 @@
- import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
+
 
 class DBClient {
   constructor() {
